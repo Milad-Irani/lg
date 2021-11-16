423df51d86eab861bd17d59eb96aadc810815ec7
vahid <Zidan.roony@yahoo.com>
2021-11-16 19:06:41 +0330

    create job app models

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ["id", "status", "created"]
+    search_fields = ("id", "status", "created")
+
+
+class PointerAdmin(admin.ModelAdmin):
+    list_display = ["id", "job", "pointer"]
+    search_fields = ("id",)
+
+
+class ClipAdmin(admin.ModelAdmin):
+    list_display = ["id", "job", "pointer", "created"]
+    search_fields = ("id", "created")
+
+
+admin.site.register(models.Job, JobAdmin)
+admin.site.register(models.Pointer, PointerAdmin)
+admin.site.register(models.Clip, ClipAdmin)
--- job/models.py
+++ job/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 # Create your models here.
+
+class StatusChoice(models.TextChoices):
+    UNPRC = "UNPRC", _("Unprocessed")
+    UNDPRC = "UNDPRC", _("UnderProcess")
+    PRC = "PRC", _("Processed")
+    FLD = "FLD", _("Failed")
+    CNL = "CNL", _("Canceled")
+
+
+class Job(models.Model):
+    status = models.CharField(
+        max_length=10, choices=StatusChoice.choices, default=StatusChoice.UNPRC
+    )
+    video_url = models.URLField(max_length=400, null = True , unique = True , blank = False)
+    created = models.DateTimeField(default=timezone.now)
+
+
+class Pointer(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    pointer = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["job", "pointer"], name="pointer-conflict"
+            )
+        ]
+
+
+class Clip(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
+    pointer = models.ForeignKey(Pointer, on_delete=models.SET_NULL, null=True)
+    clip_url = models.URLField(max_length=400, default="")
+    created = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["job", "pointer"], name="clip-conflict"
+            )
+        ]
