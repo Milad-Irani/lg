5b092ed34a2d89753a59d688ce4131bc74c1571f
Milad Arab <ahmad.arab@sap.com>
2021-12-12 10:30:39 +0000

    db tables

---
+++ sql/streams_table.sql
@@ -0,0 +1,37 @@
+-- Table: public.streams
+
+-- DROP TABLE public.streams;
+
+CREATE TABLE
+IF NOT EXISTS streams
+(
+    stream_id bigint NOT NULL,
+    video_id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    game_id bigint NOT NULL,
+    game_name character varying
+(100) COLLATE pg_catalog."default",
+    title character varying
+(400) COLLATE pg_catalog."default",
+    thumbnail_url character varying
+(400) COLLATE pg_catalog."default",
+    video_description text COLLATE pg_catalog."default",
+    view_count integer DEFAULT 0,
+    created_at date DEFAULT now
+(),
+    published_at date,
+    video_url character varying
+(400) COLLATE pg_catalog."default",
+    status character varying
+(10) COLLATE pg_catalog."default" NOT NULL,
+    failure_count integer NOT NULL DEFAULT 0,
+    failure_reason text COLLATE pg_catalog."default",
+    CONSTRAINT streams_pkey PRIMARY KEY
+(stream_id)
+
+)
+WITH
+(
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
